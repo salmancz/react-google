da85e16e76077fa0df0280e4f6a7084427be1bb0
salmancz <salman.jsdev@gmail.com>
2022-05-24 21:51:43 +0530

    staged

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      {/* Header Component*/}
+      <Header />
+      {/* Body Component*/}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="navbar__container">
+      <div className="left__navbar">
+        <div className="logo">Salman</div>
+      </div>
+      <div className="right__navbar">
+        <ul className="nav__item">
+          <li className="nav__elements"></li>
+          <li className="nav__elements"></li>
+          <li className="nav__elements"></li>
+          <li className="nav__elements"></li>
+          <li className="nav__elements"></li>
+          <li className="nav__elements"></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
